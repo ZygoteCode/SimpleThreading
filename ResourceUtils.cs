be75f3468cbc580858da35ebc9e04d5183c85d13
Gabriele Bologna <bolognagabriele2003@gmail.com>
2025-02-01 16:45:57 +0100

    First commit

---
+++ ResourceUtils.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+
+namespace SimpleThreading
+{
+    public class ResourceUtils
+    {
+        public static int Increment(ref int value) => Interlocked.Increment(ref value);
+
+        public static int Decrement(ref int value) => Interlocked.Decrement(ref value);
+
+        public static int Add(ref int value, int amount) => Interlocked.Add(ref value, amount);
+
+        public static int Exchange(ref int target, int newValue) => Interlocked.Exchange(ref target, newValue);
+
+        public static int CompareExchange(ref int target, int newValue, int comparand)
+            => Interlocked.CompareExchange(ref target, newValue, comparand);
+
+        public static long Increment(ref long value) => Interlocked.Increment(ref value);
+
+        public static long Decrement(ref long value) => Interlocked.Decrement(ref value);
+
+        public static long Add(ref long value, long amount) => Interlocked.Add(ref value, amount);
+
+        public static long Exchange(ref long target, long newValue) => Interlocked.Exchange(ref target, newValue);
+
+        public static long CompareExchange(ref long target, long newValue, long comparand)
+            => Interlocked.CompareExchange(ref target, newValue, comparand);
+
+        public static bool TrySet(ref int target, int newValue, int expectedValue)
+        {
+            return Interlocked.CompareExchange(ref target, newValue, expectedValue) == expectedValue;
+        }
+
+        public static bool TrySet(ref long target, long newValue, long expectedValue)
+        {
+            return Interlocked.CompareExchange(ref target, newValue, expectedValue) == expectedValue;
+        }
+    }
+}
---
+++ Semaphores/ResourceFairSemaphore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimpleThreading.Semaphores
+{
+    public class ResourceFairSemaphore : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore;
+        private readonly ConcurrentQueue<TaskCompletionSource<bool>> _queue = new ConcurrentQueue<TaskCompletionSource<bool>>();
+        private bool _disposed = false;
+
+        public ResourceFairSemaphore(int maxResources = 1)
+        {
+            _semaphore = new SemaphoreSlim(maxResources, maxResources);
+        }
+
+        public async Task<bool> LockResourceAsync(int timeoutMilliseconds = Timeout.Infinite)
+        {
+            var tcs = new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+            _queue.Enqueue(tcs);
+
+            if (_queue.TryPeek(out var first) && first == tcs)
+            {
+                if (await _semaphore.WaitAsync(timeoutMilliseconds))
+                {
+                    _queue.TryDequeue(out _);
+                    return true;
+                }
+                else
+                {
+                    _queue.TryDequeue(out _);
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public void UnlockResource()
+        {
+            _semaphore.Release();
+        }
+
+        public bool IsResourceAvailable() => _semaphore.CurrentCount > 0;
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _semaphore.Dispose();
+                _disposed = true;
+            }
+        }
+    }
+
+}
