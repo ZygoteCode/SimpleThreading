ebfdc448911de43b6219dad41e6cf61cacd6c8dd
ZygoteCode <zygotecode@proton.me>
2025-04-20 22:01:23 +0200

    First commit

---
+++ Semaphores/ResourceSimpleSemaphore.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace SimpleThreading.Semaphores
+{
+    public class ResourceSimpleSemaphore
+    {
+        private volatile int state = 1;
+
+        public bool IsResourceAvailable() => state == 1;
+
+        public bool LockResource()
+        {
+            return Interlocked.CompareExchange(ref state, 0, 1) == 1;
+        }
+
+        public void UnlockResource()
+        {
+            Interlocked.Exchange(ref state, 1);
+        }
+    }
+}
---
+++ Semaphores/ResourceSuperSemaphore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleThreading.Semaphores
+{
+    public class ResourceSemaphore : IDisposable
+    {
+        private readonly SemaphoreSlim _semaphore;
+        private bool _disposed = false;
+
+        public ResourceSemaphore(int maxResources = 1)
+        {
+            _semaphore = new SemaphoreSlim(maxResources, maxResources);
+        }
+
+        public bool LockResource(int timeoutMilliseconds = Timeout.Infinite)
+        {
+            return _semaphore.Wait(timeoutMilliseconds);
+        }
+
+        public void UnlockResource()
+        {
+            _semaphore.Release();
+        }
+
+        public bool IsResourceAvailable() => _semaphore.CurrentCount > 0;
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _semaphore.Dispose();
+                _disposed = true;
+            }
+        }
+    }
+}
